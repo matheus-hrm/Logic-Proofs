670a0bc731e5e420438ca1c731bbdb7a68130872
matheus-hrm <rodriguesm673@gmail.com>
2025-02-10 15:23:49 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+# language: nix
+{
+  description = "A flake for Haskell development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    hsPkgs = pkgs.haskellPackages;
+  in {
+    # A package that compiles main.hs into an executable
+    packages.${system}.haskell-app = pkgs.stdenv.mkDerivation {
+      pname = "haskell-app";
+      version = "0.1.0";
+      src = ./.;
+      buildInputs = [ hsPkgs.ghc ];
+      # The build phase compiles main.hs using GHC.
+      phases = [ "buildPhase" "installPhase" ];
+      buildPhase = ''
+        ghc -O2 -o haskell-app main.hs
+      '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp haskell-app $out/bin/
+      '';
+    };
+
+    # Set the default package to the haskell-app executable.
+    defaultPackage.${system} = self.packages.${system}.haskell-app;
+
+    # A dev shell that provides GHC for interactive development.
+    devShells.${system}.haskell = pkgs.mkShell {
+      buildInputs = [ hsPkgs.ghc ];
+    };
+  };
+}
